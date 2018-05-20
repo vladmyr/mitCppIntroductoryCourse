2c4f155e1a4fd131a978a5ac51c82c1de41395dd
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-05-20 17:59:39 +0200

    Lecture003 -- Done

--- lecture003/Main.cpp
+++ lecture003/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,6 +9,10 @@ int raiseToPower (int base, int exponent) {
 		result = result * base;
 	}
 	return result;
+}
+
+double fourthRoot (double num) {
+	return sqrt (sqrt (num));
 }
 
 /**
@@ -34,11 +39,25 @@ int fibonacci (int n) {
 	return fibonacci (n - 2) + fibonacci (n - 1);
 }
 
+/**
+ * Pass by reference
+ */
+void increment (int &a) {
+	a++;
+}
+
+void swap (int &a, int &b) {
+	int t = a;
+	a = b;
+	b = t;
+}
+
 int main () {
 	int threeExpFour = raiseToPower (3, 4);
 	int sixExpFive = raiseToPower (6, 5);
 	int twelveExpTen = raiseToPower (12, 10);
 
+	// function overloading
 	printOnNewLine ((char *)"3^4 is:");
 	printOnNewLine (threeExpFour);
 	
@@ -48,11 +67,30 @@ int main () {
 	printOnNewLine ((char *)"12^10 is:");
 	printOnNewLine (twelveExpTen);
 
+	// recursion
 	printOnNewLine ((char *)"Fibonacci value of index 5 is:");
 	printOnNewLine (fibonacci (5));
 
 	printOnNewLine ((char *)"Fibonacci value of index 14 is:");
 	printOnNewLine (fibonacci (14));
+
+	// pass by reference
+	int i = 0;
+	increment (i);
+	printOnNewLine ((char *) "Incrementing i in a function by passing by reference");
+	printOnNewLine (i);
+
+	int j = 12;
+	int k = 29;
+	printOnNewLine ((char *) "Swapping values using references");
+	cout << "before: (" << j << ", " << k << ") ";
+	swap (j, k);
+	cout << "after: (" << j << ", " << k << ")" << endl;
+
+	// using libraries
+	int l = 16;
+	printOnNewLine ((char *) "Square root of variable 'l' is:");
+	printOnNewLine (fourthRoot (l));
 
 	cin.clear();
 	cin.ignore (32767, '\n');
